120b2cd92d291b211b2f116c990ecb9bb2fb806b
SprainedSpark89 <87820585+SprainedSpark89@users.noreply.github.com>
2025-01-05 15:05:52 -0800

    i think i added b1.7-preview support

--- src/main/java/btw/lowercase/viasnapshot/SnapshotProtocolVersion.java
+++ src/main/java/btw/lowercase/viasnapshot/SnapshotProtocolVersion.java
@@ -1,15 +1,31 @@
 package btw.lowercase.viasnapshot;
 
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import com.viaversion.viaversion.api.protocol.version.SubVersionRange;
+import com.viaversion.viaversion.api.protocol.version.VersionType;
 import com.viaversion.viaversion.protocol.RedirectProtocolVersion;
 
+import net.raphimc.vialegacy.api.LegacyProtocolVersion;
+
 public class SnapshotProtocolVersion {
+	public static final ProtocolVersion b1_7_preview = registerLegacy(VersionType.BETA_LATER, 13, "b1.7-preview");
     public static final ProtocolVersion v15w31a = registerSnapshot1_8(49, "15w31a (Client Only)");
     public static final ProtocolVersion v15w31b = registerSnapshot1_8(50, "15w31b (Client Only)");
 
     private static ProtocolVersion registerSnapshot1_8(final int version, final String name) {
         final ProtocolVersion protocolVersion = new RedirectProtocolVersion(version, name, ProtocolVersion.v1_8);
         ProtocolVersion.register(protocolVersion);
+        return protocolVersion;
+    }
+    // vialegacy
+    private static ProtocolVersion registerLegacy(final VersionType versionType, final int version, final String name) {
+        return registerLegacy(versionType, version, name, null);
+    }
+
+    private static ProtocolVersion registerLegacy(final VersionType versionType, final int version, final String name, final SubVersionRange versionRange) {
+        final ProtocolVersion protocolVersion = new ProtocolVersion(versionType, version, -1, name, versionRange);
+        ProtocolVersion.register(protocolVersion);
+        LegacyProtocolVersion.PROTOCOLS.add(protocolVersion);
         return protocolVersion;
     }
 }
--- src/main/java/btw/lowercase/viasnapshot/ViaSnapshot.java
+++ src/main/java/btw/lowercase/viasnapshot/ViaSnapshot.java
@@ -2,10 +2,12 @@ package btw.lowercase.viasnapshot;
 
 import btw.lowercase.viasnapshot.protocol.v15w31ato1_8.Protocol15w31a_To1_8;
 import btw.lowercase.viasnapshot.protocol.v15w31bto15w31a.Protocol15w31b_To15w31a;
+import btw.lowercase.viasnapshot.protocol.b1_7_previewtob1_7.Protocolb1_7_previewtob1_7;
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.protocol.ProtocolManager;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import net.lenni0451.lambdaevents.EventHandler;
+import net.raphimc.vialegacy.api.LegacyProtocolVersion;
 import net.raphimc.viaproxy.ViaProxy;
 import net.raphimc.viaproxy.plugins.ViaProxyPlugin;
 import net.raphimc.viaproxy.plugins.events.ProtocolTranslatorInitEvent;
@@ -24,6 +26,7 @@ public class ViaSnapshot extends ViaProxyPlugin {
             final ProtocolManager protocolManager = Via.getManager().getProtocolManager();
             protocolManager.registerProtocol(new Protocol15w31a_To1_8(), SnapshotProtocolVersion.v15w31a, ProtocolVersion.v1_8);
             protocolManager.registerProtocol(new Protocol15w31b_To15w31a(), SnapshotProtocolVersion.v15w31b, SnapshotProtocolVersion.v15w31a);
+            protocolManager.registerProtocol(new Protocolb1_7_previewtob1_7(), SnapshotProtocolVersion.b1_7_preview, LegacyProtocolVersion.b1_7tob1_7_3);
             return null;
         });
     }
