b77e949401c1510422606d70f2eba250dddb3413
SprainedSpark89 <87820585+SprainedSpark89@users.noreply.github.com>
2025-01-06 17:16:54 -0800

    added more corrections for vialegacy

--- src/main/java/btw/lowercase/viasnapshot/mixins/Corrections.java
+++ src/main/java/btw/lowercase/viasnapshot/mixins/Corrections.java
@@ -38,6 +38,22 @@ public class Corrections {
         	modifiedName = "b1.9 prerelease 6-11w48a"; // 11w49a to 11w50a might have the same protocol as the others but spawn eggs
         } else if(originalName.equals("1.1")) {
         	modifiedName = "12w01a-1.1";
+        } else if(originalName.equals("1.2.1-1.2.3")) {
+        	modifiedName = "12w08a-1.2.3";
+        } else if(originalName.equals("1.2.4-1.2.5")) {
+        	modifiedName = "1.2.4-12w15a";
+        } else if(originalName.equals("1.3.1-1.3.2")) {
+        	modifiedName = "12w30c-1.3.2";
+        } else if(originalName.equals("1.4.2")) {
+        	modifiedName = "12w42b-1.4.2";
+        } else if(originalName.equals("1.4.6-1.4.7")) {
+        	modifiedName = "12w50a-1.4.7";
+        } else if(originalName.equals("1.5-1.5.1")) {
+        	modifiedName = "13w09c-1.5.1";
+        } else if(originalName.equals("1.5.2")) {
+        	modifiedName = "1.5.2 Prerelease-1.5.2";
+        } else if(originalName.equals("1.6.2")) {
+        	modifiedName = "1.6.2 Prerelease-1.6.2";
         }
         
         if(modifiedName == null) {
