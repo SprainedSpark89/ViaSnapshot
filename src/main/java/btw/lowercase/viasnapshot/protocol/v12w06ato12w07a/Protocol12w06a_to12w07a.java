3a920ddb1b503236377c014d0364ede1224b7848
SprainedSpark89 <87820585+SprainedSpark89@users.noreply.github.com>
2025-01-09 17:26:39 -0800

    v12w06a

---
+++ src/main/java/btw/lowercase/viasnapshot/protocol/v12w06ato12w07a/Protocol12w06a_to12w07a.java
@@ -0,0 +1,7 @@
+package btw.lowercase.viasnapshot.protocol.v12w06ato12w07a;
+
+import net.raphimc.vialegacy.protocol.release.r1_1tor1_2_1_3.Protocolr1_1Tor1_2_1_3;
+
+public class Protocol12w06a_to12w07a extends Protocolr1_1Tor1_2_1_3 {
+
+}
